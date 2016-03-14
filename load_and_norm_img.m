b3bd63cfb4fb09effe96f2a3a894793f4b203058
tyler boyd-meredith <jtboyd@stanford.edu>
2016-03-14 18:40:46 -0400

    ADD can specify file from stack to load in load_and_norm_img

--- load_and_norm_img.m
+++ load_and_norm_img.m
@@ -1,8 +1,10 @@
-function img = load_and_norm_img(img_path)
+function img = load_and_norm_img(img_path,zix)
 % img = load_and_norm_img(imgpath)
 % read image, subtract its minimum from all pixels, normalize it, and return it
-
-    img = imread(img_path);
+if nargin < 2
+    zix = 1;
+end
+    img = imread(img_path,zix);
     img = double(img - min(img(:)));
     img = normalizeToZeroOne(img);
 end
