559137fcae80c950d91632b4d37b340e51d05594
tyler boyd-meredith <jtboyd@stanford.edu>
2016-03-16 15:27:11 -0400

    FIX tracking best z for each block; comment
    was initializing the blks_best_z array in the for loop instead of
    outside of it.
    
    spruced up the comments a bit too.

--- get_similarity_wrapper.m
+++ get_similarity_wrapper.m
@@ -1,12 +1,22 @@
-function [] = get_similarity_wrapper(subject)
-setup;
-off_range = -50:50;
-blksz     = 100;
-z_range   = -3:3; % range around best matches to use
+function [] = get_similarity_wrapper(subj)
+% get_similarity_wrapper(subj)
+%
+% 
+%
+% input
+% subj       subject ID. eg 'J115' 
+%
 
-stack_names = dir(fullfile(data_dir,subject,'2015-*-*__post_stack_00*_AVERAGE.tif'));
+setup; % add useful paths; configure default figure text
+
+off_range = -50:50; % range of offsets to search for block matches
+blksz     = 100;    % size of blocks to divide images into
+z_range   = -3:3;   % range around best matches to use
+
+% load the z stack into a 3d array
+stack_names = dir(fullfile(data_dir,subj,'2015-*-*__post_stack_00*_AVERAGE.tif'));
 stack_date  = stack_names(1).name(1:10);
-stack_path  = fullfile(data_dir, subject, stack_names(1).name);
+stack_path  = fullfile(data_dir, subj, stack_names(1).name);
 stack       = load_stack(stack_path);
 
 % divide into blocks (define centers in reference to the stack images)
@@ -14,36 +24,39 @@ stack_info  = imfinfo(stack_path);
 blk_cntrs   = make_blocks(stack_info(1).Height, stack_info(1).Width, blksz);
 nblks       = length(blk_cntrs.x);
 
-slice_names = dir(fullfile(data_dir,subject,['2015-*__L01__AVERAGE.tif']));
-slice_names = {slice_names(:).name};
-ndays       = length(slice_names);
+% get a list of movies each corresponding to a day of imaging
+movie_names = dir(fullfile(data_dir,subj,['2015-*__L01__AVERAGE.tif']));
+movie_names = {movie_names(:).name};
+ndays       = length(movie_names);
 
-% TODO add rotation
-% pre-allocate correlations array and best z choice array
-C = nan(nblks, length(off_range), length(off_range), length(stack_info));
+% pre-allocate best z choice array
 blks_best_z = nan(nblks,ndays);
 
 for di = 1:ndays
-
-    % load image
-    img_path = fullfile(data_dir,subject,slice_names{di});
-    img_name = slice_names{di};
+    % pre-allocate correlations array 
+    C = nan(nblks, length(off_range), length(off_range), length(stack_info));
+    
+    % load first frame of this day's movie
+    img_path = fullfile(data_dir,subj,movie_names{di});
+    img_name = movie_names{di};
     img_date = img_name(1:10);
     img = load_and_norm_img(img_path);
     [imgy imgx] = size(img);
 
-    % if it's the first image, find the best match in the stack and 
+    % let us know what we're image we're working on
+    fprintf('\n\nworking on %s...', img_name);
+
+    % if it's the first day, find the best match in the stack and 
     % guess that it's the best z for all blocks
     if di == 1
         best_z = match_to_stack(stack, img);
         blks_best_z(:,1) = best_z;
     end
 
-
     % get cross correlation for each block
     for bi = 1:nblks
-
-        % create block indices are this block center
+        fprintf('\n\tblock %i...', bi);
+        % create block indices around this block center
         blk_yix = [(blk_cntrs.y(bi) - blksz/2):(blk_cntrs.y(bi) + blksz/2)];
         blk_xix = [(blk_cntrs.x(bi) - blksz/2):(blk_cntrs.x(bi) + blksz/2)];
        
@@ -54,26 +67,29 @@ for di = 1:ndays
 
         % select block
         block   = img(blk_yix,blk_xix);
-        %figure(1); clf; imshow(block);  
         
         % determine the neighborhood of z-values to look at
         z_to_check = blks_best_z(bi,di) + z_range;
-
         % get the cross correlation of this block against the stack image
         [c xoff yoff]   = get_xyz_similarity(stack(:,:,z_to_check), block, blk_cntrs.x(bi), blk_cntrs.y(bi), off_range);
 
         C(bi,:,:,z_to_check) = c;
+
         % update best z with the z that has the max correlation
         [~, maxix] = max(c(:));
         [~, ~, zix] = ind2sub(size(c),maxix);
+        fprintf('\n\tfavorite z for this block in neighborhood around %i...', blks_best_z(bi, di));
         blks_best_z(bi,di) = z_to_check(zix); 
         if bi < nblks
             blks_best_z(bi,di+1) = blks_best_z(bi,di);
         end
-    end
+        fprintf('s %i', blks_best_z(bi, di));
 
+    end
+    % now let's save this correlation matrix
+    savepath = fullfile(data_dir,subj,[img_name(1:end-4) '_correlations.mat']); 
+    save(savepath, 'C','off_range')
 end
-keyboard;
 
 
 end
