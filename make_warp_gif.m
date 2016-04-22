229ee20ddd98c51f5355a127a8c731879ae0b8c9
tyler boyd-meredith <jtboyd@stanford.edu>
2016-04-22 18:56:47 -0400

    FIX colormap for warp gif

--- make_warp_gif.m
+++ make_warp_gif.m
@@ -14,7 +14,8 @@ function [im] = make_warp_gif(cmat_dir, cmat_names, gifname)
 
 ndays   = length(cmat_names);
 gifname = fullfile(cmat_dir, [gifname '.gif']);
-
+b = listfonts;
+set(0,'defaulttextfontname',b{1});
 for di = 1:ndays
     cmat_path = fullfile(cmat_dir,cmat_names{di});
     if exist(cmat_path,'file')
@@ -24,15 +25,18 @@ for di = 1:ndays
     [~,maxZ] = max(best_corr_z,[],2);
     grid_size = sqrt(size(maxZ,1));
     assert(0==mod(grid_size,1));
-        
-    figure(1);clf;surf(reshape(maxZ,grid_size, grid_size))
-
+    maxZ = reshape(maxZ, grid_size, grid_size);
+    maxZ = maxZ([2:(grid_size-1)],[2:(grid_size-1)]);    
+    h = figure(1);clf;surf(maxZ)
+    set(h,'color','w');
     zlim([1 51]);
-    title(strrep(cmat_names{di},'_',' ')); 
+    colormap redbluecmap;
+    grid off;
+    title(strrep(cmat_names{di},'_',' '),'FontName','Arial'); 
     colorbar;
-    caxis([1 51]);
-
-    f = getframe(figure(1));
+    caxis([24 42 ]);
+    set(gca,'zdir','reverse');
+    f.cdata = hardcopy(h, '-Dzbuffer', '-r0');
     if di == 1
         [im, map] = rgb2ind(f.cdata,256,'nodither');
         im(1,1,1,ndays) = 0;
