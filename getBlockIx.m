f37eec01753a196e82315c814f2cee24416447d3
tyler boyd-meredith <jtboyd@stanford.edu>
2016-05-18 18:27:25 -0400

    FIX dimension flip in get block ix

--- getBlockIx.m
+++ getBlockIx.m
@@ -2,5 +2,5 @@ function [yy, xx]  = getBlockIx(blockLocBin)
 % [yy xx] = getBlockIx(blockLocBin)
 % find max across all columns and all rows of a binary matrix to get
 % indices of the nonzero entries
-    yy = find(max(blockLocBin,[],2));
-    xx = find(max(blockLocBin,[],1));
+    yy = find(max(blockLocBin,[],1));
+    xx = find(max(blockLocBin,[],2));
