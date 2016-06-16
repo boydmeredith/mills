28bca8f528b8a60e9665d2453aa9c036106ff51d
tyler boyd-meredith <jtboyd@stanford.edu>
2016-06-15 22:27:58 -0400

    Update path adder for cluster

--- addJeffCode2Path.m
+++ addJeffCode2Path.m
@@ -1,5 +1,7 @@
 if exist('/Volumes/jlgauthi','dir')
     addpath(genpath('/Volumes/jlgauthi/code'));
+elseif exist('/usr/people/jlgauthi/code','dir')
+    addpath(genpath('/usr/people/jlgauthi/code'));
 else
     fprintf('ERROR: directory not mounted\n');
 end
