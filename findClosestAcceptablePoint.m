cbe6587a6516526f54a3e23a53d0be6a39312b93
tyler boyd-meredith <jtboyd@stanford.edu>
2016-06-21 11:00:51 -0400

    AUG findClosestAcceptable point; return if already acceptable

--- findClosestAcceptablePoint.m
+++ findClosestAcceptablePoint.m
@@ -7,6 +7,13 @@ function [ii,jj] = findClosestAcceptablePoint(initialPoint,acceptableZone)
 
 [nY,nX] = size(acceptableZone);
 
+if acceptableZone(round(initialPoint(1)),round(initialPoint(2)))
+    ii = initialPoint(1);
+    jj = initialPoint(2);
+    return
+end
+
+
 % get distance to every point
 % this line is slow:
 % dists = sqrt(bsxfun(@plus,((1:nX)-initialPoint(2)).^2,((1:nY)-initialPoint(1))'.^2));
