ae192760bbc01d47938664d84a7ac3ef580ab6dd
tyler boyd-meredith <jtboyd@stanford.edu>
2016-06-23 15:21:07 -0400

    AUG take block struct or logical location matrix as input; don't fetch struct if unecessary

--- rotateAndSelectBlock.m
+++ rotateAndSelectBlock.m
@@ -1,26 +1,33 @@
-function [block] = rotateAndSelectBlock(movieFrame, blockLoc, angle);
-% block = rotateAndReselectBlock(movieFrame, blockLoc, angle) 
+function [block] = rotateAndSelectBlock(movieFrame, bInf, angle)
+% block = rotateAndReselectBlock(movieFrame, bInf, angle) 
 % 
-% Rotate movieFrame clockwise about the center of the blockLoc by the
-% specified angle and return the new image in the blockLoc
+% Rotate movieFrame clockwise about the center of the bInf.blockLoc by the
+% specified angle and return the new image in the bInf.blockLoc
 %
 % movieFrame -          An image from which to select the block 
-% blockLoc   -          A logical matrix of the same size as movieFrame with
-%                       1's indicating the position of the bloc
+% bInf       -          Structure containing a logical matrix blockLoc of the same
+%                       size as movieFrame with 1's indicating the position of the block 
+%                       The struct also has the center coordinates and
+%                       dimensions of the block. If user just passes in the
+%                       logical blockLoc matrix, the struct will be
+%                       generated.
 % angle      -          An angle in degrees by which to rotate the image
 %
 % Note: assumes that margins have been built into the edges of the
-% blockLocations so that the rotation will not introduce empty pixels to
+% bInf.blockLocations so that the rotation will not introduce empty pixels to
 % the blockT = maketform('affine',[cosd(r) sind(r) 0; -sind(r) cosd(r) 0; 0 0 1]);
 
 % get dimensions of the movie frame
 [movieFrameHeight movieFrameWidth] = size(movieFrame);
 
-% ensure the movie frame and block location matrix have same dimensions
-assert(isequal(size(blockLoc), size(movieFrame)));
-
 % get the indices, dimensions, and center of the block in the movie Frame
-bInf = getBlockInf(blockLoc);
+if islogical(bInf)
+    bInf = getBlockInf(bInf);
+end
+
+% ensure the movie frame and block location matrix have same dimensions
+assert(isequal(size(bInf.blockLoc), size(movieFrame)));
+
 
 T = maketform('affine',[cosd(angle) sind(angle) 0; -sind(angle) cosd(angle) 0; 0 0 1]);
 
