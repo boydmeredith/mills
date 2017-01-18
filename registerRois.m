db703d6b46a69bbdb7a183d26064bbd9e56959c8
tyler boyd-meredith <jtboyd@stanford.edu>
2017-01-18 17:56:35 -0500

    Improve registerRois
    - decrease leash size
    - add options to normalize the stack and the cluster image

--- registerRois.m
+++ registerRois.m
@@ -1,4 +1,4 @@
-function [rois, xyzrcoClusterPeaks] = ...
+function [rois, xyzrcoClusterPeaks, params] = ...
     registerRois(subject, theDate, location, varargin)
 
 
@@ -6,8 +6,8 @@ function [rois, xyzrcoClusterPeaks] = ...
 p = inputParser;
 addParamValue(p,'whichClusters',[]);
 addParamValue(p,'whichBlocks',[]);
-addParamValue(p,'xMargin',15);
-addParamValue(p,'yMargin',15);
+addParamValue(p,'xMargin',5);
+addParamValue(p,'yMargin',5);
 addParamValue(p,'zFitStyle','poly11');
 addParamValue(p,'rFitStyle','poly11');
 addParamValue(p,'zFitRobust','Bisquare');
@@ -19,8 +19,20 @@ addParamValue(p,'refLocSumm',[]);
 addParamValue(p,'zNbrhdRange',[-2:2]);
 addParamValue(p,'rNbrhdRange',[-1:.25:1]);
 
+% normalize stack brightness
+addParamValue(p,'normalizeStack',false);
+
+% normalize brightness of block image
+addParamValue(p,'normalizeBlock',false);
+
+
+addParamValue(p,'plot',false);
+
 %addParamValue(p,'xyzrcoClustPeaks',[]);
 parse(p,varargin{:});
+
+params = p.Results;
+
 
 whichClusters = p.Results.whichClusters;
 whichBlocks = p.Results.whichBlocks;
@@ -77,8 +89,17 @@ end
 
 % load registered peaks and the path to the stack used 
 if isempty(refLocSumm)
-    refLocSumm = load(nS.referenceLocalizationFileName,'xyzrcoPeak','stackPath');
+    refLocSumm = load(nS.referenceLocalizationFileName,'xyzrcoPeak','stackPath','blockLocations');
 end
+
+
+% identify center of each block in the 1000-frame average space
+blockCentersIn1000FrameAverage = nan(nBlocks,2);
+for bb=1:nBlocks
+    ctr = getBlockInf(refLocSumm.blockLocations(:,:,bb));
+    blockCentersIn1000FrameAverage(bb,:) = [ctr.ctrX ctr.ctrY];
+end
+
 
 % load stack and get its dimensions
 stackFileName = fullfile(PATH_DATA, refLocSumm.stackPath);
@@ -89,6 +110,24 @@ stackSz = size(stack);
 % initialize output variables
 xyzrcoClusterPeaks = nan(6, nClusts, nBlocks);
 rois(nClusts, nBlocks) = struct('w',[],'x',[],'y',[],'z',[]);
+
+
+% store values for debuggin
+cxList = nan(max(whichClusters),max(whichBlocks));
+cyList = nan(max(whichClusters),max(whichBlocks));
+leashCenters = nan(length(whichClusters),nBlocks,4);
+
+
+
+if p.Results.normalizeStack
+    stackNorm = zeros(size(stack));
+    for zz=1:size(stack,3)
+        stackNorm(:,:,zz) = normalizeImageBrightness(double(stack(:,:,zz)));
+    end
+else
+    stackNorm = stack;
+end
+
 
 for cc = whichClusters
 
@@ -105,9 +144,31 @@ for cc = whichClusters
     z=squeeze(refLocSumm.xyzrcoPeak(3,:,thisClustFrames));
     r=squeeze(refLocSumm.xyzrcoPeak(4,:,thisClustFrames));
     
+    % identify offset between 1000-frame average space and the 512x512 space
+    %   (load safe zone boundaries and use their initial values as offsets
+    %   for the current block location centers)
+    mc = load(nS.motCorrFileName);
+    safeZoneX = computeMotionCorrectionBounds(mc.xShifts,[1 512],[1 512]);
+    safeZoneY = computeMotionCorrectionBounds(mc.yShifts,[1 512],[1 512]);
+    
+    % add the offsets to the center points
+    blockCentersIn512x512(:,1) = blockCentersIn1000FrameAverage(:,1) + safeZoneX(1);
+    blockCentersIn512x512(:,2) = blockCentersIn1000FrameAverage(:,2) + safeZoneY(1);
+    
     % fit a curve to the localization for these frames
+    xForFit = repmat(blockCentersIn512x512(:,1),[1 length(thisClustFrames)]);
+    yForFit = repmat(blockCentersIn512x512(:,2),[1 length(thisClustFrames)]);
+    fitForLeashCenter_x = fit([xForFit(:) yForFit(:)], x(:),'poly22');
+    fitForLeashCenter_y = fit([xForFit(:) yForFit(:)], y(:),'poly22');
+    fitForLeashCenter_z = fit([xForFit(:) yForFit(:)], z(:),'poly22');
+    fitForLeashCenter_r = fit([xForFit(:) yForFit(:)], r(:),'poly22');
+    
+    
+    
     fitZ = fit([x(:) y(:)], z(:),p.Results.zFitStyle,'Robust',p.Results.zFitRobust);
     fitR = fit([x(:) y(:)], r(:),p.Results.rFitStyle,'Robust',p.Results.rFitRobust);
+    
+    
     
     for bb = whichBlocks
         if ~mod(bb,6)
@@ -118,7 +179,8 @@ for cc = whichClusters
         % get positioning and average image for block in the cluster
         [cframes, cx, cy, cIm]= getClusterBlockLocalization(subject,theDate,...
             location,cc,bb);
-        
+        cxList(cc,bb) = cx;
+        cyList(cc,bb) = cy;
         
         if isempty(cIm), continue, end
         
@@ -142,20 +204,38 @@ for cc = whichClusters
         assert(isequal(cImSz, [size(cellfile.rois,1), size(cellfile.rois,2)]));
         
         % set up the neighborhood info struct to constrain possible localizations
-        nbrhdInf.xCtr = cx;
-        nbrhdInf.yCtr = cy;
-        nbrhdInf.zCtr = round(fitZ(cx,cy));
-        nbrhdInf.rCtr = fitR(cx,cy);
+        nbrhdInf.xCtr = fitForLeashCenter_x(cx,cy);
+        nbrhdInf.yCtr = fitForLeashCenter_y(cx,cy);
+        nbrhdInf.zCtr = fitForLeashCenter_z(cx,cy);
+        nbrhdInf.rCtr = fitForLeashCenter_r(cx,cy);
+        
+        leashCenters(cc,bb,:) = [nbrhdInf.xCtr nbrhdInf.yCtr nbrhdInf.zCtr nbrhdInf.rCtr];
+        
+        % delete!
+        %nbrhdInf.xCtr = cx;
+        %nbrhdInf.yCtr = cy;
+        %nbrhdInf.zCtr = round(fitZ(cx,cy));
+        %nbrhdInf.rCtr = fitR(cx,cy);
+        
+        
         
         % set up a binary block location matrix that indicates the subset
         % of the cluster block image that can stand the maximal rotation
         biggestAngle = max(abs(nbrhdInf.rCtr + nbrhdInf.rOffToKeep));
         rotatableSection = makeBlockLocations(cImSz(1),cImSz(2),[1 1], 0, biggestAngle);
         
+        % normalize block image, if desired
+        if p.Results.normalizeBlock
+            cImNorm = normalizeImageBrightness(cIm);
+        else
+            cImNorm = cIm;
+        end
+        
         % localize this cluster block to the stack
         xyzrcoClusterPeaks(:,cc,bb) = localizeBlockInStackNbrhd(rotatableSection,...
-            cIm, stack, nbrhdInf);
+            cImNorm, stackNorm, nbrhdInf);
         
+        % extract values at peak correlation
         bestAngle = xyzrcoClusterPeaks(4,cc,bb);
         bestXCtr = xyzrcoClusterPeaks(1,cc,bb);
         bestYCtr = xyzrcoClusterPeaks(2,cc,bb);
@@ -170,31 +250,94 @@ for cc = whichClusters
         roiWLoc = true(size(roiWPadded,1),size(roiWPadded,2));
         %nonNanLoc(~isnan(roiWPadded(:,:,1))) = true;
         
-        roiWPaddedSz = size(roiWPadded);
         
+        % transform each ROI
+        roiWPaddedSz = size(roiWPadded);
         rotatedRoiW = nan(roiWPaddedSz);
         for ww = 1:roiWPaddedSz(3)
             rotatedRoiW(:,:,ww) = rotateAndSelectBlock(roiWPadded(:,:,ww),...
                 roiWLoc,bestAngle);
         end
         
+        % transform baseline image
+        cImPadded = padarray(cIm,padSz,nan,'both');
+        blockBaselineTransformed = rotateAndSelectBlock(cImPadded,roiWLoc,bestAngle);
+        
+        % store in output variable
         rois(cc,bb).w = rotatedRoiW;
         rois(cc,bb).x = repmat(bestXCtr + meanCenter(1:roiWPaddedSz(2)),...
             roiWPaddedSz(1),1);
         rois(cc,bb).y = repmat(bestYCtr + meanCenter(1:roiWPaddedSz(1))'...
             , 1,roiWPaddedSz(2));
         rois(cc,bb).z =  repmat(bestZ,roiWPaddedSz(1),roiWPaddedSz(2));
+        rois(cc,bb).baseline = blockBaselineTransformed;
+        
+        % plot, if desired
+        if ~isempty(p.Results.plot)
+            figure(p.Results.plot);clf
+            plot(leashCenters(cc,:,1),leashCenters(cc,:,2),'.')
+            hold on;plot(squeeze(xyzrcoClusterPeaks(1,cc,:)),squeeze(xyzrcoClusterPeaks(2,cc,:)),'ro')
+            axis ij
+            drawnow
+        end
+        
     end
     
+    % interpolate z value at each pixel by fitting Z
     xThisCluster = reshape(xyzrcoClusterPeaks(1,cc,:),[],1);
     yThisCluster = reshape(xyzrcoClusterPeaks(2,cc,:),[],1);
     zThisCluster = reshape(xyzrcoClusterPeaks(3,cc,:),[],1);
     fitZ = fit([xThisCluster yThisCluster ], zThisCluster,p.Results.zFitStyle,'Robust',p.Results.zFitRobust);
     
+    % store result
     for bb=whichBlocks
         rois(cc,bb).zFit = fitZ(rois(cc,bb).x,rois(cc,bb).y);
     end
+    
+
 end
 
 
 
+
+
+%%
+if 0 % for debugging
+    
+    %%
+    figure;hist(leashCenters(cc,:,2)' - squeeze(xyzrcoClusterPeaks(2,cc,:)),50)
+    
+    %%
+    
+    stackNorm = zeros(size(stack));
+    for zz=1:size(stack,3)
+        stackNorm(:,:,zz) = normalizeImageBrightness(double(stack(:,:,zz)));
+    end
+    %%
+    figure(101);clf
+    plot(leashCenters(cc,:,1),leashCenters(cc,:,2),'.')
+    hold on;plot(squeeze(xyzrcoClusterPeaks(1,cc,:)),squeeze(xyzrcoClusterPeaks(2,cc,:)),'ro')
+    axis image
+    
+    %%
+    
+    
+    %%
+    
+    
+    
+    figure;
+    plot(cxList(cc,:),cyList(cc,:),'.')
+
+    
+    xErrors = leashCenters(cc,:,1)' - xThisCluster;
+    yErrors = leashCenters(cc,:,2)' - yThisCluster;
+    zErrors = leashCenters(cc,:,3)' - zThisCluster;
+    
+    figure;
+    plot(abs(zErrors), abs(xErrors),'o')
+    
+    text(abs(zErrors), abs(xErrors),strsplit(num2str(1:36))')
+    ylabel('x error')
+    xlabel('z error')
+end
